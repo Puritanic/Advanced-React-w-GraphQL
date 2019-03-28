13968db5ecca6962ef9effef71b35793f168f2b2
Darkø Tasevski <darko.tasevski@redbox.com>
2019-03-28 16:21:39 +0100

    fix: Fix prisma generator

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const Mutations = {};
+const Mutations = {
+	async createItem(parent, args, ctx, info) {
+		// TODO: Check if the user is logged in
+		// CreateItem returns promise
+		const item = await ctx.db.mutation.createItem(
+			{
+				data: { ...args },
+			},
+			info
+		);
+
+		return item;
+	},
+};
 
 module.exports = Mutations;
