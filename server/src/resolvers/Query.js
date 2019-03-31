7e29b504452aaf3c90e33023f52876e8920aa615
Darkø Tasevski <darko.tasevski@redbox.com>
2019-03-31 14:45:41 +0200

    doc: Document forwardTo behaviour;

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    /**
+     * @desc If you just want to forward a query to the exact same underlying prisma query, you can use forwardTo
+     * which means that we don't have any custom logic for queries, just fetching or pushing items
+     * This also means that queries are the same both on Prisma and on Yoga server
+     * @see https://github.com/prisma/prisma-binding#forwardto
+     */
+    items: forwardTo('db'),
+};
 
 module.exports = Query;
